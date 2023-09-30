a3e507e76836f7cf739ff36934eb6a4920585569
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-30 20:17:38 +0200

    Initial Commit

---
+++ UE/UEApp.swift
@@ -0,0 +1,17 @@
+//
+//  UEApp.swift
+//  UE
+//
+//  Created by Нурбек Болат on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct UEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
